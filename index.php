ca5ff5e38948eb72943233ea59c751480a4be95e
EL HOUSAYNY Abdelhafid <abdelhafid.elhousayny@gmail.com>
2017-05-23 20:44:15 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>Test PHP</title>
+  <h1>hi<h1>
+ </head>
+ <body>
+ <?php echo '<p>Bonjour tout le monde</p>';
+ 
+for($i=1;$i<5;$i++)
+echo 'hello';
+ 
+ 
+ ?>
+ </body>
+</html>
---
+++ page1.php
@@ -0,0 +1,17 @@
+<html>
+	<body>
+		<br>
+
+		<h1 class="t1" align="center">
+			Bienvenue sur 
+		</h1>
+		<h2 class="t3" align="center">
+			CS Trace Analysis 
+		</h2>
+
+		<div align="center">
+			<img src="images.jpg" width="600px" height="300px" />
+		</div>
+
+	</body>
+</html>
